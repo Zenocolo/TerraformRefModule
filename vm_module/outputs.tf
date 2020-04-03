1d040b3a0e92077120b17ee6b1aca583e5e495dd
Zenocolo <phzenhae@microsoft.com>
2020-04-03 11:45:15 +0200

    Updated Output.tf to test releases
    Updated Output.tf to test releases

--- vm_module/outputs.tf
+++ vm_module/outputs.tf
@@ -1,9 +1,9 @@
 output "private-ip" {
   value       = "${azurerm_network_interface.main.*.private_ip_address}"
-  description = "Private IP Address"
+  description = "Private IP Address Ref 1.0.1"
 }
 
 output "public-ip" {
   value       = "${azurerm_public_ip.main.*.ip_address}"
-  description = "Public IP Address"
+  description = "Public IP Address Ref 1.0.1"
 }
