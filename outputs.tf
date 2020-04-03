599f0e10eed511de8624406e5f905fe3c62bc0d5
Zenocolo <phzenhae@microsoft.com>
2020-04-03 12:53:08 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "private-ip" {
   value       = "${azurerm_network_interface.main.*.private_ip_address}"
-  description = "Private IP Address"
+  description = "Private IP Address v2.0.1"
 }
 
 output "public-ip" {
   value       = "${azurerm_public_ip.main.*.ip_address}"
-  description = "Public IP Address"
+  description = "Public IP Address v2.0.1"
 }
