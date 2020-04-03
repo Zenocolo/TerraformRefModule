3846e699c6967d3601c7ceae057507513c6d8460
zenocolo <phzenhae@microsoft.com>
2020-04-03 11:58:44 +0200

    xx

--- vm_module/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "private-ip" {
-  value       = "${azurerm_network_interface.main.*.private_ip_address}"
-  description = "Private IP Address Ref 1.0.1"
-}
-
-output "public-ip" {
-  value       = "${azurerm_public_ip.main.*.ip_address}"
-  description = "Public IP Address Ref 1.0.1"
-}
